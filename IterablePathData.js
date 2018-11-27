c9792f4e92db037ad1b7da6fd499d60479aabf82
radishengine <radishengine@gmail.com>
2018-11-27 14:34:16 +0000

    Update IterablePathData.js

--- IterablePathData.js
+++ IterablePathData.js
@@ -227,6 +227,188 @@ define(function() {
     return {x:Px, y:Py};
   }
   
+  // curve flattening code below adapted from
+  // Anti-Grain Geometry 2.4 by Maxim Shemanarev
+  // (Modified BSD License)
+
+  function squareDistance(x1,y1, x2,y2) {
+    const dx = x2-x1, dy = y2-y1;
+    return dx*dx + dy*dy;
+  }
+
+  function* flattenCubic(x1,y1, x2,y2, x3,y3, x4,y4, options) {
+    const curve_distance_epsilon = 1e-30;
+    const curve_collinearity_epsilon = 1e-30;
+    const curve_angle_tolerance_epsilon = 0.01;
+    const curve_recursion_limit = 32;
+
+    options = options || {};
+    const m_distance_tolerance_square = Math.pow(0.5 / (options.approximationScale || 1), 2);
+    const m_angle_tolerance = options.angleTolerance || 0;
+    const m_cusp_limit = options.cuspLimit || 0;
+
+    function* recurse(x1,y1, x2,y2, x3,y3, x4,y4, level) {
+      if (level > curve_recursion_limit) return;
+
+      // Calculate line segment mid-points
+      const x12   = (x1 + x2) / 2;
+      const y12   = (y1 + y2) / 2;
+      const x23   = (x2 + x3) / 2;
+      const y23   = (y2 + y3) / 2;
+      const x34   = (x3 + x4) / 2;
+      const y34   = (y3 + y4) / 2;
+      const x123  = (x12 + x23) / 2;
+      const y123  = (y12 + y23) / 2;
+      const x234  = (x23 + x34) / 2;
+      const y234  = (y23 + y34) / 2;
+      const x1234 = (x123 + x234) / 2;
+      const y1234 = (y123 + y234) / 2;
+
+      // Try to approximate the full cubic curve by a single straight line
+      const dx = x4 - x1;
+      const dy = y4 - y1;
+
+      const d2 = Math.abs((x2 - x4)*dy - (y2 - y4)*dx);
+      const d3 = Math.abs((x3 - x4)*dy - (y3 - y4)*dx);
+      var da1, da2, k;
+
+      switch (((d2 > curve_collinearity_epsilon) << 1) | (d3 > curve_collinearity_epsilon)) {
+        case 0:
+          // all collinear OR p1===p4
+          k = dx*dx + dy*dy;
+          if (k === 0) {
+            d2 = squareDistance(x1, y1, x2, y2);
+            d3 = squareDistance(x4, y4, x3, y3);
+          }
+          else {
+            k   = 1 / k;
+            da1 = x2 - x1;
+            da2 = y2 - y1;
+            d2  = k * (da1*dx + da2*dy);
+            da1 = x3 - x1;
+            da2 = y3 - y1;
+            d3  = k * (da1*dx + da2*dy);
+            if (d2 > 0 && d2 < 1 && d3 > 0 && d3 < 1) {
+              // Simple collinear case, 1---2---3---4
+              // We can leave just two endpoints
+              return;
+            }
+            d2 = (d2 <= 0) ? squareDistance(x2, y2, x1, y1)
+               : (d2 >= 1) ? squareDistance(x2, y2, x4, y4)
+               : squareDistance(x2, y2, x1 + d2*dx, y1 + d2*dy);
+            d3 = (d3 <= 0) ? squareDistance(x3, y3, x1, y1)
+               : (d3 >= 1) ? squareDistance(x3, y3, x4, y4)
+               : squareDistance(x3, y3, x1 + d3*dx, y1 + d3*dy);
+          }
+          if (d2 > d3) {
+            if (d2 < m_distance_tolerance_square) {
+              yield {type:'L', values:[x2, y2]};
+              return;
+            }
+          }
+          else if (d3 < m_distance_tolerance_square) {
+            yield {type:'L', values:[x3, y3]};
+            return;
+          }
+          break;
+
+        case 1:
+          // p1,p2,p4 are collinear, p3 is significant
+          if (d3*d3 <= m_distance_tolerance_square * (dx*dx + dy*dy)) {
+            if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+              yield {type:'L', values:[x23, y23]};
+              return;
+            }
+
+            // Angle Condition
+            da1 = Math.abs(Math.atan2(y4 - y3, x4 - x3) - Math.atan2(y3 - y2, x3 - x2));
+            if (da1 >= Math.PI) da1 = 2*Math.PI - da1;
+
+            if (da1 < m_angle_tolerance) {
+              yield {type:'L', values:[x2, y2]};
+              yield {type:'L', values:[x3, y3]};
+              return;
+            }
+
+            if (m_cusp_limit !== 0 && da1 > m_cusp_limit) {
+              yield {type:'L', values:[x3, y3]};
+              return;
+            }
+          }
+          break;
+
+        case 2:
+          // p1,p3,p4 are collinear, p2 is significant
+          if (d2 * d2 <= m_distance_tolerance_square * (dx*dx + dy*dy)) {
+            if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+              yield {type:'L', values:[x23, y23]};
+              return;
+            }
+
+            // Angle Condition
+            da1 = Math.abs(Math.atan2(y3 - y2, x3 - x2) - Math.atan2(y2 - y1, x2 - x1));
+            if (da1 >= Math.PI) da1 = 2*Math.PI - da1;
+
+            if (da1 < m_angle_tolerance) {
+              yield {type:'L', values:[x2, y2]};
+              yield {type:'L', values:[x3, y3]};
+              return;
+            }
+
+            if (m_cusp_limit !== 0 && da1 > m_cusp_limit) {
+              yield {type:'L', values:[x2, y2]};
+              return;
+            }
+          }
+          break;
+
+        case 3: 
+          // Regular case
+          if ((d2 + d3)*(d2 + d3) <= m_distance_tolerance_square * (dx*dx + dy*dy)) {
+            // If the curvature doesn't exceed the distance_tolerance value
+            // we tend to finish subdivisions.
+            if (m_angle_tolerance < curve_angle_tolerance_epsilon) {
+              yield {type:'L', values:[x23, y23]};
+              return;
+            }
+
+            // Angle & Cusp Condition
+            k   = Math.atan2(y3-y2, x3-x2);
+            da1 = Math.abs(k - Math.atan2(y2-y1, x2-x1));
+            da2 = Math.abs(Math.atan2(y4-y3, x4-x3) - k);
+            if (da1 >= Math.PI) da1 = 2*Math.PI - da1;
+            if (da2 >= Math.PI) da2 = 2*Math.PI - da2;
+
+            if (da1 + da2 < m_angle_tolerance) {
+              // Finally we can stop the recursion
+              yield {type:'L', values:[x23, y23]};
+              return;
+            }
+
+            if (m_cusp_limit !== 0) {
+              if (da1 > m_cusp_limit) {
+                yield {type:'L', values:[x2, y2]};
+                return;
+              }
+              if (da2 > m_cusp_limit) {
+                yield {type:'L', values:[x3, y3]};
+                return;
+              }
+            }
+          }
+          break;
+      }
+
+      // Continue subdivision
+      yield* recurse(x1, y1, x12, y12, x123, y123, x1234, y1234, level + 1);
+      yield* recurse(x1234, y1234, x234, y234, x34, y34, x4, y4, level + 1);
+    }
+
+    yield* recurse(x1,y1, x2,y2, x3,y3, x4,y4, 0);
+    yield {type:'L', values:[x4, y4]};
+  }
+  
+  
   function PathState() {
   }
   PathState.prototype = {
@@ -837,6 +1019,29 @@ define(function() {
         value: iter,
       });
       return iter;
+    },
+    get asNormalized() {
+      return this.asSimpleParams.asAbsolute.asCubicOnly.asUnreflected;
+    },
+    flatten: function(options) {
+      const self = this.asNormalized;
+      return new IterablePathData(function*() {
+        var state = new PathState;
+        for (var step of self) {
+          if (step.type === 'C') {
+            yield* flattenCubic(
+              state.x, state.y,
+              step.values[0], step.values[1],
+              step.values[2], step.values[3],
+              step.values[4], step.values[5],
+              options);
+          }
+          else {
+            yield step;
+          }
+          state.update(step);
+        }
+      });
     },
   };
   
